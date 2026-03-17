9cae93c4e6ab6d528b62aaa013cb6e7f69ff2a13
codebefore <info@codebefore.com>
2026-03-17 21:19:12 +0300

    Sort match skills by relevance

--- lib/widgets/match/match_skill_cloud.dart
+++ lib/widgets/match/match_skill_cloud.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:ytnkio/globals/global_texts.dart';
+import 'package:ytnkio/models/match/score_item.dart';
 import 'package:ytnkio/widgets/match/match_skill_chip.dart';
 import 'package:ytnkio/models/match/match.dart';
+
+int _getSkillPriority(ScoreItem skill) {
+  final isRelevant = skill.isRelevant ?? true;
+  if (!isRelevant) {
+    return 3;
+  }
+
+  if (skill.score >= 80) {
+    return 0;
+  }
+
+  if (skill.score >= 60) {
+    return 1;
+  }
+
+  return 2;
+}
+
+List<ScoreItem> _sortSkillFitness(List<ScoreItem> skills) {
+  final sortedSkills = [...skills];
+  sortedSkills.sort((left, right) {
+    final priorityDiff = _getSkillPriority(left) - _getSkillPriority(right);
+    if (priorityDiff != 0) {
+      return priorityDiff;
+    }
+
+    final scoreDiff = right.score.compareTo(left.score);
+    if (scoreDiff != 0) {
+      return scoreDiff;
+    }
+
+    return left.name.toLowerCase().compareTo(right.name.toLowerCase());
+  });
+  return sortedSkills;
+}
 
 class MatchSkillCloud extends StatelessWidget {
   const MatchSkillCloud({required this.match, super.key});
@@ -10,6 +46,8 @@ class MatchSkillCloud extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final sortedSkills = _sortSkillFitness(match.skillFitness);
+
     return Card(
       color: Colors.white,
       elevation: 0,
@@ -33,7 +71,7 @@ class MatchSkillCloud extends StatelessWidget {
               spacing: 8.0,
               runSpacing: 2.0,
               children: [
-                ...match.skillFitness.map((skill) => MatchSkillChip(
+                ...sortedSkills.map((skill) => MatchSkillChip(
                   skillName: skill.name,
                   score: skill.score,
                   isRelevant: skill.isRelevant ?? true,
